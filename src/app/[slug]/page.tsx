25fbf89fb996e50a7fa233ddcf4dfe4a86424f3b
RomarioNik <115925888+RomarioNik@users.noreply.github.com>
2025-07-05 19:51:04 +0100

    initial commit

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,36 @@
+import { createReader } from "@keystatic/core/reader";
+import React from "react";
+import Markdoc from "@markdoc/markdoc";
+
+import keystaticConfig from "../../../keystatic.config";
+
+const reader = createReader(process.cwd(), keystaticConfig);
+
+export default async function Post({ params }: { params: { slug: string } }) {
+  const post = await reader.collections.posts.read(params.slug);
+
+  if (!post) {
+    return <div>No Post Found</div>;
+  }
+
+  const { node } = await post.content();
+  const errors = Markdoc.validate(node);
+
+  if (errors.length) {
+    console.error(errors);
+    throw new Error("Invalid content");
+  }
+
+  const renderable = Markdoc.transform(node);
+
+  return (
+    <>
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-4">
+        <h1>{post.title}</h1>
+        {Markdoc.renderers.react(renderable, React)}
+        <hr />
+        <a href={`/`}>Back to Posts</a>
+      </div>
+    </>
+  );
+}
---
+++ src/app/keystatic/layout.tsx
@@ -0,0 +1,9 @@
+import KeystaticApp from "./keystatic";
+
+export default function Layout() {
+  return (
+    <div className="border-amber-50">
+      <KeystaticApp />
+    </div>
+  );
+}
