18ad7056df19f82bd0ba6b4b86c143085ba1daac
RomarioNik <115925888+RomarioNik@users.noreply.github.com>
2025-07-06 02:09:26 +0100

    fix route

--- src/app/api/[...params]/route.ts
+++ src/app/api/[...params]/route.ts
@@ -3,6 +3,14 @@ import config from "../../../../keystatic.config";
 
 export const dynamic = "force-static";
 
+export async function generateStaticParams() {
+  const { POST, GET } = makeRouteHandler({
+    config,
+  });
+
+  return [{ POST, GET }];
+}
+
 export const { POST, GET } = makeRouteHandler({
   config,
 });
