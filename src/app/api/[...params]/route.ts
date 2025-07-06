a1251d4bee01b38c452988b6a6f2db6dcf9d9095
RomarioNik <115925888+RomarioNik@users.noreply.github.com>
2025-07-06 01:59:50 +0100

    dynamic route

--- src/app/api/[...params]/route.ts
+++ src/app/api/[...params]/route.ts
@@ -1,6 +1,8 @@
 import { makeRouteHandler } from "@keystatic/next/route-handler";
 import config from "../../../../keystatic.config";
 
+export const dynamic = "force-static";
+
 export const { POST, GET } = makeRouteHandler({
   config,
 });
