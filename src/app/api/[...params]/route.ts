c83474a782e236a10165ce040c74f2f481f9f3ca
RomarioNik <115925888+RomarioNik@users.noreply.github.com>
2025-07-06 02:39:29 +0100

    fix-8 route

--- src/app/api/[...params]/route.ts
+++ src/app/api/[...params]/route.ts
@@ -2,6 +2,7 @@ import { makeRouteHandler } from "@keystatic/next/route-handler";
 import config from "../../../../keystatic.config";
 
 export const dynamic = "force-static";
+export const dynamicParams = true;
 
 export async function generateStaticParams() {
   // console.log("params :", await params);
